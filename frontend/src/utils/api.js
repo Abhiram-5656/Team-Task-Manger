4de8942ffc87a8e6815fbd5cbeebe2548fd802f6
Abhiram <2200031123@kluniversity.in>
2026-07-22 15:59:27 +0530

    Update API base URL for production environment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Correct for React (CRA)
 const BASE_URL =
-  process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+  process.env.REACT_APP_API_URL || 'https://team-task-manger-dm9c.onrender.com';
 
 const API = axios.create({
   baseURL: BASE_URL,
