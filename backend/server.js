c104a2bcbea3bb083ef7a67d2baf18352699afa2
Abhiram <2200031123@kluniversity.in>
2026-07-22 16:52:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL || 'https://ttm9.netlify.app/',
   credentials: true
 }));
 app.use(express.json());
